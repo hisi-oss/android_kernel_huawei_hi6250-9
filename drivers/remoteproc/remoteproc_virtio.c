e494b3efced09e9807d8280bf2f59539d486cd4d
R0rt1z2 <me@r0rt1z2.com>
2024-02-12 23:06:51 +0100

    drivers: remoteproc: Import Huawei changes

--- drivers/remoteproc/remoteproc_virtio.c
+++ drivers/remoteproc/remoteproc_virtio.c
@@ -137,6 +137,9 @@ static void __rproc_virtio_del_vqs(struct virtio_device *vdev)
 static void rproc_virtio_del_vqs(struct virtio_device *vdev)
 {
 	__rproc_virtio_del_vqs(vdev);
+#ifdef CONFIG_HISI_REMOTEPROC
+	vdev->config->reset(vdev);
+#endif
 }
 
 static int rproc_virtio_find_vqs(struct virtio_device *vdev, unsigned int nvqs,
@@ -189,7 +192,10 @@ static void rproc_virtio_reset(struct virtio_device *vdev)
 
 	rsc = (void *)rvdev->rproc->table_ptr + rvdev->rsc_offset;
 
+#ifndef CONFIG_HISI_REMOTEPROC
 	rsc->status = 0;
+#endif
+
 	dev_dbg(&vdev->dev, "reset !\n");
 }
 
