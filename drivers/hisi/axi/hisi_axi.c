9a798243458dd8913d4f38e5a7132c49dec349a8
R0rt1z2 <me@r0rt1z2.com>
2024-02-15 18:28:06 +0100

    drivers: hisi: axi: Don't define `CONFIG_HISI_AXI_TEST`

--- drivers/hisi/axi/hisi_axi.c
+++ drivers/hisi/axi/hisi_axi.c
@@ -7,8 +7,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#define CONFIG_HISI_AXI_TEST
-
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/interrupt.h>
